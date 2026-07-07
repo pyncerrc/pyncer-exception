20b03d485c22289127053dc84edd42a9a69c4498
pyncer <root@pyncer.com>
2026-07-07 18:33:43 -0400

    Added BadMethodCallException and BadFunctionCallException.

---
+++ src/BadFunctionCallException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Pyncer\Exception;
+
+use Pyncer\Exception\LogicException;
+
+class BadFunctionCallException extends LogicException
+{}
---
+++ src/BadMethodCallException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Pyncer\Exception;
+
+use Pyncer\Exception\BadFunctionCallException;
+
+class BadMethodCallException extends BadFunctionCallException
+{}
